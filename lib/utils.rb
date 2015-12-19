d0013f90a6ef10175ec6bc5c00da7315a17ceef8
Ruud Verbeek <ruud@arfie.nl>
2015-12-19 21:37:21 +0100

    Allow arrays in Utils.symbolize_hash

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,11 +1,18 @@
 module Daemobot
   module Utils
     def self.symbolize_hash(hash)
-      hash.inject({}) do |res, (key, value)|
-        new_key = (key.is_a? String) ? key.to_sym : key
-        new_value = (value.is_a? Hash) ? symbolize_hash(value) : value
-        res[new_key] = new_value
-        res
+      if hash.is_a? Hash
+        hash.inject({}) do |res, (key, value)|
+          new_key = (key.is_a? String) ? key.to_sym : key
+          new_value = (value.is_a? Hash) ? symbolize_hash(value) : value
+          res[new_key] = new_value
+          res
+        end
+      elsif hash.is_a? Array
+        hash.inject([]) do |res, value|
+          res << ((value.is_a? Hash) ? symbolize_hash(value) : value)
+          res
+        end
       end
     end
   end
